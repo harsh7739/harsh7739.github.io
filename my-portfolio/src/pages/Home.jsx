50431155ef34c568e80b0277f367b2d0fb9cbd1a
harsh7739 <hrbhumi123@gmail.com>
2023-10-31 19:06:33 +0530

    added

--- my-portfolio/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-function Home(){
-    return (
-        <div id="home">
-             <h1 id="user-detail-name">Harshranjan kumar</h1>
-             <p id="user-detail-intro">Full Stack Web Developer</p>
-        </div>
-    )
-}
-export default Home
